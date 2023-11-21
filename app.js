1136a11551ca819ab571d1a08da02db817d6cdcc
Yue Wu <yuewu@Godfather.local>
2023-11-20 18:30:47 -0800

    change

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import "dotenv/config";
-import session from "express-session";
-
 const app = express();
 app.use(cors());
 app.use(express.json());
