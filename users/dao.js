d711af23936c4802201fffb04bbe55c2c5f0077f
Yue Wu <yuewu@Godfather.local>
2023-12-05 22:37:55 -0800

    a6

--- users/dao.js
+++ users/dao.js
@@ -4,8 +4,7 @@ export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) =>
   model.findOne({ username: username });
-export const findUserByCredentials = (username, password) =>
-  model.findOne({ username, password });
+  export const findUserByCredentials = (usr, pass) => model.findOne({ username: usr, password: pass });
 export const updateUser = (userId, user) =>
   model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
--- users/routes.js
+++ users/routes.js
@@ -1,5 +1,4 @@
 import * as dao from "./dao.js";
-let currentUser = null;
 function UserRoutes(app) {
   const createUser = async (req, res) => {
     const user = await dao.createUser(req.body);
